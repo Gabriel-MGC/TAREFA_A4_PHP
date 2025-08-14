0ac5e53dac09349f2bd87d4dfa85537c2459167c
Gabriel-MGC <condegabriel703@gmail.com>
2025-08-13 21:29:30 -0300

    Add files via upload

---
+++ desafio2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Números de 100 a 200</title>
+</head>
+<body>
+    <h1>Números de 100 a 200 com incremento de 2 em 2</h1>
+    <?php
+    for ($i = 100; $i <= 200; $i += 2) {
+        echo $i . "<br>";
+    }
+    ?>
+</body>
+</html>
---
+++ desafio3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Números Ímpares de 500 a 1000</title>
+</head>
+<body>
+    <h1>Números Ímpares de 500 a 1000</h1>
+    <?php
+    for ($i = 501; $i <= 1000; $i += 2) {
+        echo $i . "<br>";
+    }
+    ?>
+</body>
+</html>
